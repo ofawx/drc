acf7a0a3f02160a2d437f674109ed38077469107
Oscar Fawkes <oscar@outoftime.co>
2019-06-11 22:12:20 +1000

    refactor & mvp

--- drc.js
+++ drc.js
@@ -1,6 +1,15 @@
+let unfilterString = "All";
+
+let classSelect = document.getElementById("class-select");
+let distSelect = document.getElementById("dist-select");
+classSelect.addEventListener('change', setResultsTable);
+distSelect.addEventListener('change', setResultsTable);
+
 var raw_results = {"results":[]};
 var processedResults = [];
 var table;
+
+
 
 var _table_ = document.createElement('table'),
   _tr_ = document.createElement('tr'),
@@ -50,17 +59,19 @@ function readSingleFile(e) {
     }
     var reader = new FileReader();
     reader.onload = function(e) {
-      var contents = e.target.result;
-      parseJSON(contents);
+      newJSON(e.target.result);
     };
     reader.readAsText(file);
   }
 }
 
+function newJSON(contents) {
+  parseJSON(contents);
+  setResultsTable();
+  updateFilters();
+}
+
 function parseJSON(contents) {
-  //var element = document.getElementById('file-content');
-  //element.textContent = contents;
-  console.log(JSON.parse(contents).results)
   var newresults = JSON.parse(contents).results;
   for (var i = 0; i < newresults.length; i++) {
     var willAdd = true;
@@ -74,8 +85,6 @@ function parseJSON(contents) {
       raw_results.results = raw_results.results.concat(JSON.parse(newJSON));
     }
   }
-
-  setResultsTable();
 }
 
 function setResultsTable() {
@@ -84,16 +93,30 @@ function setResultsTable() {
   for (let results of raw_results.results) {
     for (let raceName of Object.keys(results)) {
       for (let participant of results[raceName].participants) {
-        let resultentry = participantResult(participant);
-        if (resultentry) {
-          processedResults = processedResults.concat([resultentry]);
+        let result = JSON.parse(JSON.stringify(participant));
+        if (checkResultMatch(result)) {
+          processedResults = processedResults.concat([processResult(result)]);
         } else {
           continue;
         }
       }
     }
   }
-  processedResults.sort((a,b) => a["Total Time"] - b["Total Time"]);
+
+
+  processedResults.sort((a,b) => a.timeTotal - b.timeTotal);
+  var lastTime = 0, lastPlace = 0;
+  for (var i = 0; i < processedResults.length; i++) {
+    var result = processedResults[i];
+    if (result.timeTotal > lastTime) {
+      lastPlace = i+1;
+      result.overallPlace = lastPlace;
+      lastTime = result.timeTotal;
+    } else {
+      result.overallPlace = lastPlace;
+    }
+    processedResults[i] = formatResult(result);
+  }
 
   let elementResults = document.getElementById("results_table")
   if (table) {
@@ -103,33 +126,98 @@ function setResultsTable() {
   elementResults.appendChild(table);
 }
 
-function participantResult(participant) {
+function processResult(result) {
+  // Find total time from sum of splits
+  result.timeTotal = 0;
+  for (var i = 0; i < result.splits.length; i++) {
+    result.timeTotal += result.splits[i].split_time;
+  }
+
+  return result;
+}
+
+function checkResultMatch(result) {
+  if (result.splits) {
+  } else {
+    return false;
+  }
+
+  let classSelected = classSelect.options[classSelect.selectedIndex].text
+  if (classSelected == result.class || classSelected == unfilterString) {
+  } else {
+    return false;
+  }
+
+  let distSelected = distSelect.options[distSelect.selectedIndex].text
+  if (distSelected == result.score || distSelected == unfilterString) {
+  } else {
+    return false;
+  }
+
+  // Check result against filters
+  return true;
+}
+
+function formatResult(participant) {
   var modified = {};
 
+  modified["Place"] = participant.overallPlace;
+
   modified["Name"] = participant.participant;
+
+  modified["Time"] = decisecondFormatter(participant.timeTotal);
+
+  modified["Distance"] = participant.score;
 
   modified["Class"] = participant.class;
 
   modified["Split Avg"] = participant.avg_pace;
 
-  // Format splits nicely and find total time
-  modified["Total Time"] = 0;
-  if (participant.splits) {
-    for (var i = 0; i < participant.splits.length; i++) {
-      modified["Split "+String(i+1)] = decisecondFormatter(participant.splits[i].split_time);
-      modified["Total Time"] += participant.splits[i].split_time/10;
-    }
-    delete(modified.splits);
-  } else {
-    return;
-  }
-  modified["Total Time"] = modified["Total Time"].toFixed(1);
-
   modified["SPM Avg"] = participant.spm;
+
+  // Format splits nicely and find total time
+  for (var i = 0; i < participant.splits.length; i++) {
+    let split = participant.splits[i];
+    modified["Split "+String(i+1)] = decisecondFormatter(split.split_time) + " @ " + split.split_stroke_rate;
+  }
+
+
+
+
+
 
 
 
   return modified;
+}
+
+function updateFilters() {
+  updateFilter(distSelect, "score");
+  updateFilter(classSelect, "class");
+}
+
+function updateFilter(selector, key) {
+  var arr = [];
+  for (let results of raw_results.results) {
+    for (let raceName of Object.keys(results)) {
+      for (let participant of results[raceName].participants) {
+        arr = arr.concat([participant[key]]);
+      }
+    }
+  }
+  arr = arr
+  arr = [unfilterString].concat([...new Set(arr)].sort(function (a, b) {
+    return ('' + a.attr).localeCompare(b.attr);
+  }));
+  for (var i = 0; i < selector.options.length; i++) {
+    selector.options.remove(selector.options[i]);
+  }
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == "") continue;
+    var opt = document.createElement("option");
+    opt.text = opt.value = arr[i];
+    selector.options.add(opt);
+  }
 }
 
 function decisecondFormatter(deci) {
@@ -137,7 +225,7 @@ function decisecondFormatter(deci) {
   let mins = Math.floor(secs/60);
   let rsecs = Math.floor(secs%60);
   let rdeci = deci%10;
-  return ""+mins+":"+rsecs+"."+rdeci;
+  return ""+mins+":"+(rsecs<10?"0":"")+rsecs+"."+rdeci;
 }
 
 document.getElementById('file-input').addEventListener('change', readSingleFile, false);
